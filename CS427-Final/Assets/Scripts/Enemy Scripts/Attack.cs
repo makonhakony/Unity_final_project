07c83585e29ef94e0f057bfc0fa9ae8565f97f90
hcthanh <hcthanh137@gmail.com>
2019-08-21 21:35:05 +0700

    Thanh's commit

---
+++ CS427-Final/Assets/Scripts/Enemy Scripts/Attack.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+    public float damage = 2f;
+    public float radius = 1f;
+    public LayerMask layerMask;
+	
+	void Update () {
+
+        Collider[] hits = Physics.OverlapSphere(transform.position, radius, layerMask);
+
+        if(hits.Length > 0) {
+            print("We touched: " + hits[0].gameObject.tag);
+            hits[0].gameObject.GetComponent<PlayerHealth>().ApplyDamage(damage);
+
+            gameObject.SetActive(false);
+
+        }
+
+	}
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CS427-Final/Assets/Scripts/Enemy Scripts/PlayerHealth.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    private EnemyAnimator enemy_Anim;
+    private NavMeshAgent navAgent;
+    private EnemyController enemy_Controller;
+
+    public float health = 100f;
+
+    public bool is_Boar, is_Cannibal;
+
+    private bool is_Dead;
+
+    private EnemyAudio enemyAudio;
+
+    void Awake()
+    {
+
+        if (is_Boar || is_Cannibal)
+        {
+            enemy_Anim = GetComponent<EnemyAnimator>();
+            enemy_Controller = GetComponent<EnemyController>();
+            navAgent = GetComponent<NavMeshAgent>();
+
+            // get enemy audio
+            enemyAudio = GetComponentInChildren<EnemyAudio>();
+        }
+    }
+
+    public void ApplyDamage(float damage)
+    {
+
+        // if we died don't execute the rest of the code
+        if (is_Dead)
+            return;
+
+        health -= damage;
+
+        if (is_Boar || is_Cannibal)
+        {
+            if (enemy_Controller.Enemy_State == EnemyState.PATROL)
+            {
+                enemy_Controller.chase_Distance = 50f;
+            }
+        }
+
+        if (health <= 0f)
+        {
+
+            PlayerDied();
+
+            is_Dead = true;
+        }
+
+    } // apply damage
+
+    void PlayerDied()
+    {
+        if (is_Cannibal)
+        {
+            GetComponent<Animator>().enabled = false;
+            GetComponent<BoxCollider>().isTrigger = false;
+            GetComponent<Rigidbody>().AddTorque(-transform.forward * 5f);
+
+            enemy_Controller.enabled = false;
+            navAgent.enabled = false;
+            enemy_Anim.enabled = false;
+
+            StartCoroutine(DeadSound());
+
+            // EnemyManager spawn more enemies
+            EnemySpawn.instance.EnemyDied(true);
+        }
+
+        if (is_Boar)
+        {
+
+            navAgent.velocity = Vector3.zero;
+            navAgent.isStopped = true;
+            enemy_Controller.enabled = false;
+
+            enemy_Anim.Dead();
+
+            StartCoroutine(DeadSound());
+
+            // EnemyManager spawn more enemies
+            EnemySpawn.instance.EnemyDied(false);
+        }
+        Invoke("TurnOffGameObject", 3f);
+    } // player died
+
+    void TurnOffGameObject()
+    {
+        gameObject.SetActive(false);
+    }
+
+    IEnumerator DeadSound()
+    {
+        yield return new WaitForSeconds(0.3f);
+        enemyAudio.Play_DeadSound();
+    }
+
+} // class
