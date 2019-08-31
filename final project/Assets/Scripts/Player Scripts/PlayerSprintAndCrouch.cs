4fa1df23a8a46ce6a83b29d08ff4707f0ebd10d6
An 'HAKONY' Phạm <32831923+makonhakony@users.noreply.github.com>
2019-08-31 20:09:51 +0700

    update version

---
+++ final project/Assets/Scripts/Player Scripts/PlayerSprintAndCrouch.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSprintAndCrouch : MonoBehaviour
+{
+    private PlayerMovement playerMovement;
+    public float sprint_speed = 10f;
+    public float move_speed = 5f;
+    public float crouch_speed = 2f;
+
+    private Transform look_root;
+    private float stand_height = 1.6f;
+    private float crouch_height = 1f;
+
+    private bool isCrouching;
+
+    private PlayerFootsteps player_footstep;
+
+    private float sprint_volume = 1f;
+    
+    private float crouch_volume = 0.1f;
+
+    private float walk_volume_min = 0.2f , walk_volume_max =0.6f;
+
+    private float walk_step_distance = 0.4f;
+
+    private float sprint_step_distance = 0.25f;
+
+    private float crouch_step_distance = 0.5f;
+    private PlayerStats player_Stats;
+    private float sprint_Value = 100f;
+    public float sprint_Treshold = 10f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        look_root = transform.GetChild(0);
+
+        player_footstep = GetComponentInChildren<PlayerFootsteps>();
+        player_Stats = GetComponent<PlayerStats>();
+    }
+
+    void Start()
+    {
+        player_footstep.volume_max = walk_volume_max;
+        player_footstep.volume_min = walk_volume_min;
+        player_footstep.step_distance = walk_step_distance;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Sprint();
+        Crouch();
+    }
+
+    void Sprint(){
+        // if we have stamina we can sprint
+        if(sprint_Value > 0f) {
+
+            if (Input.GetKeyDown(KeyCode.LeftShift) && !isCrouching) {
+
+                playerMovement.speed = sprint_speed;
+
+                player_footstep.step_distance = sprint_step_distance;
+                player_footstep.volume_max = sprint_volume;
+                player_footstep.volume_min = sprint_volume;
+
+            }
+
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftShift) && !isCrouching) {
+
+            playerMovement.speed = move_speed;
+
+            player_footstep.step_distance = walk_step_distance;
+            player_footstep.volume_min = walk_volume_min;
+            player_footstep.volume_max = walk_volume_max;
+
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift) && !isCrouching) {
+
+            sprint_Value -= sprint_Treshold * Time.deltaTime;
+
+            if(sprint_Value <= 0f) {
+
+                sprint_Value = 0f;
+
+                // reset the speed and sound
+                playerMovement.speed = move_speed;
+                player_footstep.step_distance = walk_step_distance;
+                player_footstep.volume_min = walk_volume_min;
+                player_footstep.volume_max = walk_volume_max;
+
+
+            }
+
+            player_Stats.Display_StaminaStats(sprint_Value);
+
+        } else {
+
+            if(sprint_Value != 100f) {
+
+                sprint_Value += (sprint_Treshold / 2f) * Time.deltaTime;
+
+                player_Stats.Display_StaminaStats(sprint_Value);
+
+                if(sprint_Value > 100f) {
+                    sprint_Value = 100f;
+                }
+
+            }
+
+        }
+    }
+
+    void Crouch(){
+        if (Input.GetKeyDown(KeyCode.C)){
+            if (isCrouching){
+                look_root.localPosition = new Vector3(0f,stand_height,0f);
+                playerMovement.speed = move_speed;
+
+                player_footstep.volume_max = walk_volume_max;
+                player_footstep.volume_min = walk_volume_min;
+                player_footstep.step_distance = walk_step_distance;
+
+                isCrouching = false;
+            }
+            else {
+                look_root.localPosition = new Vector3(0f,crouch_height,0f);
+                playerMovement.speed = crouch_speed;
+
+                player_footstep.volume_max = crouch_volume;
+                player_footstep.volume_min = crouch_volume;
+                player_footstep.step_distance = crouch_step_distance;
+
+                isCrouching = true;
+            }
+        }
+        
+    }
+}
---
+++ final project/Assets/Scripts/Weapon Scripts/ArrowAndBow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowAndBow : MonoBehaviour
+{
+    private Rigidbody myBody;
+
+    public float speed = 30f;
+    public float deactivate_Timer = 3f;
+    public float damage = 15f;
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DeactivateGameObject",deactivate_Timer);
+    }
+    public void Launch(Camera mainCamera){
+        myBody.velocity = mainCamera.transform.forward*speed;
+
+        transform.LookAt(transform.position + myBody.velocity);
+    }
+
+    // Update is called once per frame
+    void DeactivateGameObject(){
+        if (gameObject.activeInHierarchy){
+            gameObject.SetActive(false);
+        }
+    }
+    void OnTriggerEnter(Collider target){
+        print("haha");
+        //after touch enemy ==> de active
+        if(target.tag == Tag.ENEMY_TAG) {
+
+            target.GetComponent<PlayerHealth>().ApplyDamage(damage);
+
+            gameObject.SetActive(false);
+
+        }
+    }
+}
